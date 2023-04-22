9c1fc82a52bae7eab26d33d8cef7d66951f4251c
Paula Rodrigues <pullynnhah@icloud.com>
2023-04-22 16:53:58 -0300

    feat: form button and handle form

--- src/components/BuyersForm.js
+++ src/components/BuyersForm.js
@@ -1,22 +1,50 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { postPurchase } from "../services/api";
+import { OrangeButton } from "../styles/OrangeButton";
 import BuyerInput from "./BuyerInput";
 
 export default function BuyersForm({ buyers, setBuyers }) {
+  const navigate = useNavigate();
+
   function updateBuyers(index, buyer) {
     setBuyers(buyers.toSpliced(index, 1, buyer));
   }
 
+  function handleForm(e) {
+    e.preventDefault();
+    const ids = buyers.map(b => b.idSeat);
+    const customers = buyers.map(b => ({ idAssento: b.idSeat, nome: b.name, cpf: b.cpf }));
+    postPurchase({ ids, compradores: customers })
+      .then(res => {
+        console.log(res.data);
+        navigate(`/success`);
+      })
+      .catch(err => console.error(err.response.data));
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleForm}>
       {buyers.map((b, index) => (
         <BuyerInput key={b.idSeat} index={index} buyer={b} updateBuyers={updateBuyers} />
       ))}
+
+      <Wrapper>
+        <OrangeButton>Reservar assento(s)</OrangeButton>
+      </Wrapper>
     </Form>
   );
 }
 
 const Form = styled.form`
   width: 84vw;
-  margin: 42px auto 120px;
+  margin: 42px auto 147px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 57px;
 `;
